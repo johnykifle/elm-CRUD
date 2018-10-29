eb7264a1961b8fd665ff06c1436107397fbc3819
Yohannes Russom <johnykifle@gmail.com>
2018-10-29 13:06:05 +0100

    add return to home link

--- src/Pages/Posts.elm
+++ src/Pages/Posts.elm
@@ -1,21 +1,25 @@
 module Pages.Posts exposing (view)
 
-import Html exposing (Html, div, table, tbody, td, text, th, thead, tr)
+import Html exposing (Html, a, div, table, tbody, td, text, th, thead, tr)
+import Html.Attributes exposing (href)
 import Routes
 import Shared exposing (Msg(..), Post)
 
 
 view : List Post -> Html Msg
 view posts =
-    table []
-        [ thead []
-            [ tr []
-                [ th [] [ text "Id" ]
-                , th [] [ text "Title" ]
-                , th [] [ text "Author-Name" ]
+    div []
+        [ a [ href "/" ] [ text "Back-Home" ]
+        , table []
+            [ thead []
+                [ tr []
+                    [ th [] [ text "Id" ]
+                    , th [] [ text "Title" ]
+                    , th [] [ text "Author-Name" ]
+                    ]
                 ]
+            , tbody [] (List.map postRow posts)
             ]
-        , tbody [] (List.map postRow posts)
         ]
 
 
